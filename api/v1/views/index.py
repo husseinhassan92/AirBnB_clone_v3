319d06bbba4359b7e2f8f93ebc48b6858850824b
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-10-01 18:39:58 +0300

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,9 +7,23 @@ flask with general routes
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """return JSON of OK status"""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def count():
+    """"""
+    counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(counts)
